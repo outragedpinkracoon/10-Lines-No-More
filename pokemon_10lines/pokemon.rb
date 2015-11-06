884ad1423c7fc8cef63f54a5b5eaa4f88974715b
Valerie Gibson <vgibson14@gmail.com>
2015-11-06 08:30:54 +0000

    adds sp atk and def

--- pokemon_10lines/pokemon.rb
+++ pokemon_10lines/pokemon.rb
@@ -2,9 +2,9 @@
 client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'pokemon_db')
 db = client["pokemon_db"]
 db.drop()
-(1..2).to_a.each { |i| db.insert_one(JSON.parse(Pokegem.get "pokemon", i)) }
-puts "WHERCH PERKERMAHN DER U WERNT TER SER?"
+(1..10).to_a.each { |i| db.insert_one(JSON.parse(Pokegem.get "pokemon", i)) }
+puts "\n \n WHERCH PERKERMAHN DER U WERNT TER SER?"
 input = gets.chomp
 selected = db.find(:name => input.capitalize).first()
 translated = Ermahgerd.translate("oh my god #{selected[:name]} is a #{selected[:types].first()[:name]} pokemon!")
-puts "\n #{translated} \n Pokedex No. #{selected[:pkdx_id]} | Attack #{selected[:attack]} | Defense #{selected[:defense] } | Speed #{selected[:speed]}"
+puts "\n \n #{translated} \n Pokedex No. #{selected[:pkdx_id]} | Attack #{selected[:attack]} | Defense #{selected[:defense] } | Speed #{selected[:speed]} | SpAtk #{selected[:sp_atk]} | SpDef #{selected[:sp_def]}"
